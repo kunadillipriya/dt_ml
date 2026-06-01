136a54506c14a1e1eb1d2143bca9a642bb2ee4c5
kunadillipriya <kdillipriya6107@gmail.com>
2026-06-01 13:23:32 +0530

    first commit

--- dt_ml/__init__.py
+++ dt_ml/__init__.py
@@ -1,5 +1,5 @@
-from .data_loader import load_csv
-from .data_quality import compute_data_quality
+from dt_ml.data_loader import load_csv
+from dt_ml.data_quality import compute_data_quality
 
 __all__ = ["load_csv",
            "compute",
@@ -7,6 +7,6 @@ __all__ = ["load_csv",
           "load_csv",
            "detect_column_roles", "compute_quality_score"]
 
-from .baseline_analytics import compute
+from dt_ml.baseline_analytics import compute
 
 __all__ = ["compute"]
--- tests/test_run.py
+++ tests/test_run.py
@@ -6,4 +6,7 @@ df = pd.read_csv("validation/test_data/sales_data.csv")
 
 result = compute(df)
 
-print(result)
+print("\n",result)
+from dt_ml import compute_data_quality
+a= compute_data_quality(df)
+print("Data quality:\n",a)
